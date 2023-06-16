1237d5252f3073b823981865140d4587d99a57c7
Isaac <iico7no2@gmail.com>
2023-06-15 19:08:51 -0500

    Update getVersion

--- version/getVersion.mjs
+++ version/getVersion.mjs
@@ -1,3 +1,10 @@
+import path from 'node:path';
+import fs from 'fs-extra';
+import getRootDir from '../scripts/util/getRootDir.mjs';
+
+const rootDir = getRootDir();
+const {version} = JSON.parse(fs.readFileSync(path.join(rootDir, 'package.json')));
+
 /**
  * Entry function for get app version.
  * In current implementation, it returns `version` from `package.json`, but you can implement any logic here.
@@ -5,5 +12,5 @@
  * @return {string}
  */
 export function getVersion() {
-  return process.env.npm_package_version;
+  return version;
 }
